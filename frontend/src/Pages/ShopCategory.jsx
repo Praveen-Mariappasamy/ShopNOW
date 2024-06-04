20fb5e0d016480d268465896760f0777c5d25efb
Praveen-Mariappasamy <115199875+Praveen-Mariappasamy@users.noreply.github.com>
2024-06-04 22:22:23 +0530

    Update ShopCategory.jsx

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,10 +1,21 @@
-import React , {useContext} from 'react'
+import React , {useContext, useState} from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Item/Item'
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+
+  const {setFilter , filter , all_product} = useContext(ShopContext);
+  
+  const [sortText , setSort] = useState("Low -> High")
+
+  const sorter = ()=>{
+    if(filter===-1) setFilter(1);
+    else setFilter(-1);
+    if(sortText==="Low -> High") setSort("High -> Low");
+    else setSort("Low -> High");
+  }
+
   return (
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt="" />
@@ -12,8 +23,8 @@ const ShopCategory = (props) => {
         <p>
           <span>Showing 1-12</span> out of 36 products
         </p>
-        <div className="shopcategory-sort">
-          Sort by <img src={dropdown_icon} alt="" />
+        <div className="shopcategory-sort" onClick={sorter}>
+          {sortText} <img src={dropdown_icon}  alt="" />
         </div>
       </div>
       <div className='shopcategory-products'>
