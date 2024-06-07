bd04f0c6f19701d2158afc4c8ce9b0db10ef32c3
Praveen-Mariappasamy <115199875+Praveen-Mariappasamy@users.noreply.github.com>
2024-06-07 15:57:25 +0530

    Update Item.jsx

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Item = (props) => {
   return (
     <div className="item">
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
+      <Link to={`/product/Rs {props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
-            ${props.new_price}
+            Rs {props.new_price}
         </div>
         <div className="item-price-old">
-            ${props.old_price}
+            Rs {props.old_price}
         </div>
       </div>
     </div>
